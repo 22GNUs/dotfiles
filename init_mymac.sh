acb99add3a60b29f562b6ae1ae90480ec420b98e
wxh <stormaroon@gmail.com>
2022-07-18 16:41:22 +0800

    Add gitconfig

---
+++ init_mymac.sh
@@ -0,0 +1,12 @@
+# Install tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+
+# For homebrew
+# Install pip
+brew install python3-pip
+brew install git-delta
+
+# Install deoplete support
+pip3 install --upgrade neovim
+pip2 install --upgrade neovim
