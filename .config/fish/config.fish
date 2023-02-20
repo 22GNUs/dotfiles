44bec9dbacfadf14e13317d47bf2d691ccdcdc92
22GNUs <stormaroon@gmail.com>
2023-02-20 14:44:07 +0800

    Sync config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
+# Suppress greeting
+set fish_greeting
 # Add HomeBrew's bin directory to path so you can use HomeBrew's binaries like `starship`
 # Fish uses `fish_add_path` instead of `export PATH` modify $PATH.
 fish_add_path "/opt/homebrew/bin/"
