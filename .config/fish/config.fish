63dab8fcfa46cc527cba74efa63a40eddb2551ba
22GNUs <stormaroon@gmail.com>
2023-04-17 11:14:32 +0800

    Update neovide so now can use transparent mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@
 set fish_greeting
 # Add HomeBrew's bin directory to path so you can use HomeBrew's binaries like `starship`
 # Fish uses `fish_add_path` instead of `export PATH` modify $PATH.
-fish_add_path "/opt/homebrew/bin/"
+fish_add_path /opt/homebrew/bin/
 
 # Add cargo to path
 fish_add_path "$HOME/.cargo/bin"
@@ -10,8 +10,8 @@ fish_add_path "$HOME/.cargo/bin"
 # Enable Starship prompt
 starship init fish | source
 
-alias gvim="neovide --multigrid --frame buttonless"
+alias gvim="neovide --multigrid --frame transparent"
 
 if status is-interactive
-  # Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here
 end
