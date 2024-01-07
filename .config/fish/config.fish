efce19b40d771952b22ab12b1db631b9647ac10d
22GNUs <stormaroon@gmail.com>
2024-01-07 19:08:17 +0800

    Config default fish env

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,6 @@
 set fish_greeting
 # Add HomeBrew's bin directory to path so you can use HomeBrew's binaries like `starship`
 # Fish uses `fish_add_path` instead of `export PATH` modify $PATH.
-fish_add_path /opt/homebrew/bin/
 
 # Add cargo to path
 fish_add_path "$HOME/.cargo/bin"
@@ -15,3 +14,17 @@ alias gvim="neovide --frame transparent"
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
+    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+        . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
