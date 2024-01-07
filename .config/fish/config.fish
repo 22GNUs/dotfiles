7e977db946a061c58fa50e54277cbf87d931a154
22GNUs <stormaroon@gmail.com>
2024-01-07 19:16:43 +0800

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,9 +3,6 @@ set fish_greeting
 # Add HomeBrew's bin directory to path so you can use HomeBrew's binaries like `starship`
 # Fish uses `fish_add_path` instead of `export PATH` modify $PATH.
 
-# Add cargo to path
-fish_add_path "$HOME/.cargo/bin"
-
 # Enable Starship prompt
 starship init fish | source
 
