51c4e3cf1caa30b6ba7cc272dccd862747541713
22GNUs <stormaroon@gmail.com>
2024-01-07 23:09:03 +0800

    fish_add_path append to the last

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,15 @@
 # Suppress greeting
 set fish_greeting
+
 # Add HomeBrew's bin directory to path so you can use HomeBrew's binaries like `starship`
 # Fish uses `fish_add_path` instead of `export PATH` modify $PATH.
+# fish_add_path /opt/homebrew/bin/
+set PATH $PATH /opt/homebrew/bin/
 
 # Enable Starship prompt
 starship init fish | source
 
-alias gvim="neovide --frame transparent"
+alias gvim="neovide --multigrid --frame transparent"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
@@ -24,4 +27,3 @@ else
     end
 end
 # <<< conda initialize <<<
-
