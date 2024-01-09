cdd5b61a4a4b1a57e5a1851dbdd7d4a974ecbec9
wxh257150 <wxh257150@alibaba-inc.com>
2024-01-09 11:06:34 +0800

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,12 +4,14 @@ set fish_greeting
 # Add HomeBrew's bin directory to path so you can use HomeBrew's binaries like `starship`
 # Fish uses `fish_add_path` instead of `export PATH` modify $PATH.
 # fish_add_path /opt/homebrew/bin/
-set PATH $PATH /opt/homebrew/bin/
+if not contains /opt/homebrew/bin $PATH
+    set PATH $PATH /opt/homebrew/bin/
+end
 
 # Enable Starship prompt
 starship init fish | source
 
-alias gvim="neovide --multigrid --frame transparent"
+alias gvim="neovide --frame transparent"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
@@ -18,12 +20,12 @@ end
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
-    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" hook $argv | source
 else
     if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
         . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
     else
-        set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
+        set -x PATH /opt/homebrew/Caskroom/miniconda/base/bin $PATH
     end
 end
 # <<< conda initialize <<<
