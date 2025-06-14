be268798931d9799a21b2316acddcc9d5f8b32aa
22GNUs <stormaroon@gmail.com>
2025-06-14 15:32:42 +0800

    Update computer

--- computer.sh
+++ computer.sh
@@ -5,5 +5,22 @@
 # Install dev tools
 brew install neovim
 brew install neovide
+
 # formetter
 brew install shfmt
+
+# Install aerospace
+brew install --cask nikitabobko/tap/aerospace
+
+# Install jankyborders
+brew tap FelixKratz/formulae
+brew install borders
+
+# Install sketchybar
+brew tap FelixKratz/formulae
+brew install sketchybar
+brew install --cask font-hack-nerd-font
+mkdir -p ~/.config/sketchybar/plugins
+cp $(brew --prefix)/share/sketchybar/examples/sketchybarrc ~/.config/sketchybar/sketchybarrc
+cp -r $(brew --prefix)/share/sketchybar/examples/plugins/ ~/.config/sketchybar/plugins/
+brew services start sketchybar
