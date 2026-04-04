343df0cd45d90d45341405e280dacc91b31ff198
22GNUs <stormaroon@gmail.com>
2026-04-04 22:14:05 +0800

    feat(fish): add commit wrapper to call pi commit skill

--- .config/fish/functions/commit.fish
+++ .config/fish/functions/commit.fish
@@ -1,3 +1,3 @@
 function commit
-    pi -p "/skill:commit commit code" --model cf-compat/custom-fireworks/accounts/fireworks/routers/kimi-k2p5-turbo --no-session $argv
+    pi -p "/skill:commit commit code" --model github-copilot/gpt-5-mini --no-session $argv
 end
