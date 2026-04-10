9052f4c177c1e3ad66892e025d2c2daf24a1fa2d
22GNUs <stormaroon@gmail.com>
2026-04-10 11:08:42 +0800

    feat(nvim): add obsidian plugin and update pi agent settings
    Include new Obsidian plugin config and tweak agent settings in .pi/agent/settings.json

---
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,20 @@
+return {
+  "obsidian-nvim/obsidian.nvim",
+  version = "*", -- use latest release, remove to use latest commit
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    legacy_commands = false, -- this will be removed in the next major release
+    workspaces = {
+      {
+        name = "work",
+        path = "~/vaults/work",
+      },
+    },
+    daily_notes = {
+      folder = "daily",
+      date_format = "YYYY-MM-DD",
+      default_tags = { "daily" },
+    },
+  },
+}
