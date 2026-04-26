6c92e5b11ef28eff0eeb6971377659290c83c253
22GNUs <stormaroon@gmail.com>
2026-04-26 10:47:14 +0800

    fix: add more domains to fish NO_PROXY

--- .config/fish/functions/set_proxy.fish
+++ .config/fish/functions/set_proxy.fish
@@ -2,5 +2,9 @@ function set_proxy
     set -gx all_proxy http://my.proxy:7890
     set -gx http_proxy http://my.proxy:7890
     set -gx https_proxy http://my.proxy:7890
-    set -gx NO_PROXY localhost,127.0.0.1
+
+    # 基础：本地地址不走代理
+    # 域名后缀匹配：.example.com 匹配 *.example.com
+    # 新增域名只需追加到列表末尾
+    set -gx no_proxy localhost,127.0.0.1,.local,.internal,.cn,.aliyun.com,.alibaba-inc.com,.taobao.com,.jd.com,.bilibili.com,.qq.com,.163.com,.baidu.com
 end
