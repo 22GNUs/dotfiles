85523cc7955fb74d3493304d6ec02b7477415617
22GNUs <stormaroon@gmail.com>
2026-07-28 18:48:52 +0800

    chore(config): set git user, fix pi update npm registry, add herdr plugins lock
    Configure git user name/email for 22GNUs. Force official npm registry when running 'pi update' to bypass anpm lag. Add empty herdr plugins lock file.

--- .config/fish/functions/pi.fish
+++ .config/fish/functions/pi.fish
@@ -1,5 +1,12 @@
 function pi
     # Always launch pi in FFF override mode unless explicitly overridden by a CLI flag.
     set -x PI_FFF_MODE override
+
+    # pi update installs from npm; use the official registry so anpm lag
+    # cannot hide newly published @earendil-works/pi-coding-agent versions.
+    if test (count $argv) -ge 1; and test $argv[1] = update
+        set -x npm_config_registry https://registry.npmjs.org/
+    end
+
     command pi $argv
 end
