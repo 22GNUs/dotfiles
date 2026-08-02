6d15f8e0730d942f8c4c41fe7a41044f84ad5f9b
22GNUs <stormaroon@gmail.com>
2026-08-02 14:30:13 +0800

    chore(pi): drop fff mode override and add caffeinate config

--- .config/fish/functions/pi.fish
+++ .config/fish/functions/pi.fish
@@ -1,7 +1,4 @@
 function pi
-    # Always launch pi in FFF override mode unless explicitly overridden by a CLI flag.
-    set -x PI_FFF_MODE override
-
     # pi update installs from npm; use the official registry so anpm lag
     # cannot hide newly published @earendil-works/pi-coding-agent versions.
     if test (count $argv) -ge 1; and test $argv[1] = update
